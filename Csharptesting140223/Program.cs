c9bbbe44e7f458843b287d4fa316ab23535da902
Methuli.Dias <methuli.dias@education.nsw.gov.au>
2023-02-15 10:40:36 +1100

    92B completed added extra hello world.

--- Csharptesting140223/Program.cs
+++ Csharptesting140223/Program.cs
@@ -11,6 +11,7 @@ namespace Csharptesting140223
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            Console.WriteLine("hello world");
             Console.ReadLine();
         }
 
